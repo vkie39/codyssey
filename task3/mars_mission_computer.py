acc763251fe2bb4737f40c414ad678a5ecd1daba
vkie39 <jwsh171210@naver.com>
2025-03-24 18:36:13 +0900

    env_values생성성

--- task3/mars_mission_computer.py
+++ task3/mars_mission_computer.py
@@ -3,13 +3,11 @@ import random
 class DummySensor:
     def __init__(self):
         self.env_values = {
-            'marse_base_internal_tempreature' = random.uniform(18, 30),
-            'marse_base_external_tempreature' = random.uniform(0, 21), 
-            'maese_base_internal_humidity' = random.uniform(50, 60),
-            'marse_base_external_illuminance' = random.uniform(500, 715),
-            'marse_base_internal_co2' = random.uniform(0.02, 0.1), 
-            'marse_base_internal_oxygen' = random.uniform(4, 7)          
-            }
-        
-        
-
+            'mars_base_internal_temperature' : None,
+            'mars_base_external_temperature' : None,
+            'mars_base_internal_humidity' : None,
+            'mars_base_external_illuminance' : None,
+            'mars_base_internal_co2' : None,
+            'mars_base_internal_oxygen' : None
+        }
+    
