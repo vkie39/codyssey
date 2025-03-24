399af0dc6c6c2b481d549545d781c06b3fb8860b
vkie39 <jwsh171210@naver.com>
2025-03-24 18:59:21 +0900

    gg

--- task3/mars_mission_computer.py
+++ task3/mars_mission_computer.py
@@ -11,3 +11,20 @@ class DummySensor:
             'mars_base_internal_oxygen' : None
         }
     
+    def set_env(self):
+        self.env_values['mars_base_internal_temperature'] = random.randint(18, 30)
+        self.env_values['mars_base_external_temperature'] = random.randint(0, 21)
+        self.env_values['mars_base_internal_humidity'] = random.randint(50, 60)
+        self.env_values['mars_base_external_illuminance'] = random.randint(500, 715)
+        self.env_values['mars_base_internal_co2'] = random.uniform(0.02, 0.1)
+        self.env_values['mars_base_internal_oxygen'] = random.randint(4, 7)
+    
+    def get_env(self):
+        return self.env_values
+    
+sensor = DummySensor()
+print(sensor.env_values)
+
+ds = DummySensor()
+ds.set_env()
+print(ds.get_env())
