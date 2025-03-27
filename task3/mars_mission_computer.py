937352906bed781334360d6530ec57a8fe0618c8
vkie39 <jwsh171210@naver.com>
2025-03-27 19:25:40 +0900

    각주 추가

--- task3/mars_mission_computer.py
+++ task3/mars_mission_computer.py
@@ -1,7 +1,9 @@
 import random
 
+#클래스 생성
 class DummySensor:
     def __init__(self):
+        #딕셔너리를 담고 있는 인스턴스 변수(사전 객체)
         self.env_values = {
             'mars_base_internal_temperature' : None,
             'mars_base_external_temperature' : None,
@@ -11,6 +13,7 @@ class DummySensor:
             'mars_base_internal_oxygen' : None
         }
     
+    #랜덤으로 값 생성하는 메서드 생성
     def set_env(self):
         self.env_values['mars_base_internal_temperature'] = random.randint(18, 30)
         self.env_values['mars_base_external_temperature'] = random.randint(0, 21)
@@ -19,7 +22,9 @@ class DummySensor:
         self.env_values['mars_base_internal_co2'] = round(random.uniform(0.02, 0.1), 4)
         self.env_values['mars_base_internal_oxygen'] = random.randint(4, 7)
     
+    #값 반환하는 메서드 생성
     def get_env(self, timestamp):
+        #딕셔너리 값 로그 파일에 저장
         with open('task3/env_log.txt', 'a') as log_file:
             log_file.write(f"[{timestamp}] {self.env_values}\n")
         return self.env_values
@@ -33,11 +38,12 @@ def get_user_timestamp():
     minute = input("분 (예: 20): ")
     return f"{year}-{month}-{day} {hour}:{minute}"
 
-# 실행
+#인스턴스 생성
 ds = DummySensor()
 ds.set_env()
 timestamp = get_user_timestamp()
 
 print('----------출력----------')
-print('env_value 출력: ', ds.env_values)
-print('\nget_env 값: ', ds.get_env(timestamp))
+print('env_value 출력: ', ds.env_values) #클래스 인스턴스의 딕셔너리 참조
+print('\nset_env 값: ', ds.set_env()) #리턴값 지정 안 해서 None 출력    
+print('\ngt_env 값: ', ds.get_env()) #랜덤으로 생성된 값 출력
